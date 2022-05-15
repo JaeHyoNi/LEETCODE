76d781a53850ce8b4bab34248c3ec08fc8c64ed1
kimcharless <89509933+kimcharless@users.noreply.github.com>
2022-05-15 16:03:56 +0900

    2165번

---
+++ contest_279/2165.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long smallestNumber(long long num) {
+    if(num == 0) return 0;
+    vector<int> change;
+    while(num != 0){
+        change.emplace_back(num%10);num/=10;
+    }
+    sort(change.begin(),change.end());
+    if(change[0] == 0){
+        int i = 0; while(change[i] == 0) i++;
+        swap(change[0] , change[i]);
+    }
+    for(auto K:change){num = num*10+K;}
+    return num;
+}
+
+int main(){
+    int num = 301;
+    cout<<smallestNumber(num);
+}
